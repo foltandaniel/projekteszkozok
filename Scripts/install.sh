e2c761aa402ca08a10902b10c0c8380ad6089d48
zetya96 <z.dani@t-online.hu>
2018-03-08 17:10:20 +0100

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -7,5 +7,7 @@
 echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
 curl -o Unity.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg?_ga=2.211363349.168156340.1520523193-492440057.1520523193
 
+curl -o WinSupport.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.3.1f1.pkg
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
+sudo installer WinSupport.pkg -target /
