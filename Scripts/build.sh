5f6989b9bc815b1e306092eae63ebd621fb7fe75
zetya96 <z.dani@t-online.hu>
2018-03-08 16:53:14 +0100

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -12,6 +12,7 @@ echo "Attempting to build $project for Windows"
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
+  -buildTarget Win
   -buildWindowsPlayer "$(pwd)/Deploy/Win/$project.exe" \
   -quit
 
