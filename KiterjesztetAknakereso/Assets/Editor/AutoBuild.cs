940b0c315e00939c01a05f7bc3f7ee1e0a2934d4
zetya96 <z.dani@t-online.hu>
2018-03-09 15:58:28 +0100

    autobuild script

---
+++ KiterjesztetAknakereso/Assets/Editor/AutoBuild.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+class AutoBuild
+{
+    static void PerformBuild()
+    {
+        string[] scenes = { "Assets/Scenes/Menu.unity" };
+        BuildPipeline.BuildPlayer(scenes,".",BuildTarget.StandaloneWindows,BuildOptions.None);
+    }
+}
--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,14 +6,7 @@
 project="ci-build"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Deploy/Win/$project.exe" \
-  -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -executeMethod MyEditorScript.PerformBuild
 
 #echo "Attempting to build $project for OS X"
 #/Applications/Unity/Unity.app/Contents/MacOS/Unity \
