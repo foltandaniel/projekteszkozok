ab5c038dfa3a5285b5868dc1c3c79b1a402ff5c6
zetya96 <z.dani@t-online.hu>
2018-03-09 16:24:31 +0100

    autobuild

--- KiterjesztetAknakereso/Assets/Editor/AutoBuild.cs
+++ KiterjesztetAknakereso/Assets/Editor/AutoBuild.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEditor;
 class AutoBuild
 {
+	  [MenuItem("Tools/Clear PlayerPrefs")]
+    private static void NewMenuOption()
+    {
+        AutoBuild.PerformBuild();
+    }
     static void PerformBuild()
     {
         string[] scenes = { "Assets/Scenes/Menu.unity" };
-        BuildPipeline.BuildPlayer(scenes,".",BuildTarget.StandaloneWindows,BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes," /Users/travis/build/foltandaniel/projekteszkozok/WIN",BuildTarget.StandaloneWindows,BuildOptions.None);
     }
 }
