2f2cb397b13f8140a6063406226eada007e7c511
zetya96 <z.dani@t-online.hu>
2018-03-09 16:08:32 +0100

    autobuild

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,7 +6,14 @@
 project="ci-build"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -executeMethod MyEditorScript.PerformBuild
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -executeMethod AutoBuild.PerformBuild \
+  -quit
 
 #echo "Attempting to build $project for OS X"
 #/Applications/Unity/Unity.app/Contents/MacOS/Unity \
